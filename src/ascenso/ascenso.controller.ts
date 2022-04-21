d9cde571b33c22bbcdc102bc0295b6a9dde36219
fabian <programergitoran@gmail.com>
2022-04-21 12:55:56 -0300

    ascenso ordenado, falta probar mas, controlar no ingresar orden existente

--- src/ascenso/ascenso.controller.ts
+++ src/ascenso/ascenso.controller.ts
@@ -1,4 +1,120 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { AscensoService } from './ascenso.service';
+import { CreateAscensoDto } from './dto/create-ascenso-dto';
+import { EditAscensoDto } from './dto/edit-ascenso-dto';
+import { Ascenso } from './entities/ascenso.entity';
 
 @Controller('ascenso')
-export class AscensoController {}
+export class AscensoController {
+    constructor(
+        private readonly ascensoService: AscensoService
+    ){}
+
+     /**
+     * Petición http que lista todos los registros
+     * @returns 
+     */
+      @Get()
+      async getAll(){
+          return await this.ascensoService.getAll();
+      }
+
+      /**
+     * Petición http que devuelve un registro según id
+     * @param id 
+     * @returns 
+     */
+    @Get(':id')
+    async getOne(
+        @Param('id',ParseIntPipe)
+        id: number
+    ){
+        return await this.ascensoService.getOne(id);
+    }
+    //..........................................................
+
+    /**
+     * Petición http que crea un nuevo registro
+     * @param data 
+     * @returns 
+     */
+     @Post()
+     async create(
+         @Body()
+         data: CreateAscensoDto
+     ){
+        //LISTA DE ASCENSO PARA REORDENAR
+        let ascenso_vigente: Partial<Ascenso> = new Ascenso;
+        let ascenso_aux: Partial<Ascenso> = new Ascenso;
+        let list_ascensos: Ascenso[] = [];
+        let orden_actualizar: number=0;
+        ascenso_vigente = await this.ascensoService.getAscensoVigenteXLegajo(data.legajo);
+        if (ascenso_vigente){
+            list_ascensos = await this.ascensoService.getAscensosVigentesReOrdenar(ascenso_vigente.orden, ascenso_vigente.grado_id);
+            orden_actualizar = ascenso_vigente.orden;
+            for (let ascenso of list_ascensos){
+                console.log("ascenso", ascenso);
+                //EDICION DE NUMERO DE ORDEN 
+                ascenso_aux = {
+                    orden: orden_actualizar
+                }          
+                const respuesta_orden = await this.ascensoService.editOne(ascenso.id_ascenso, ascenso_aux);
+                //EDICION DE NUMERO DE ORDEN 
+                orden_actualizar= orden_actualizar + 1
+            }
+            
+        }
+        else{            
+            let aux = await this.ascensoService.getAscensosXLegajo(data.legajo);
+            if(aux[1] > 0){
+                //throw new NotFoundException('No se encontro un ascenso vigente para este personal');
+            }
+            console.log("cantidad", aux[1]);
+            console.log("lista ascensos", await this.ascensoService.getAscensosXLegajo(data.legajo));
+            
+        }
+        
+        //EDICION DE CAMPO VIGENTE COMO FALSO EN TODOS LOS REGISTROS DE ASCENSO DE PERSONAL
+        let data_aux: Partial<Ascenso>= new Ascenso;
+        data_aux = {
+            vigente:false
+        }          
+        const respuesta_traslado = await this.ascensoService.quitarAscensoVigente(data.legajo, data_aux);
+        //fin EDICION DE CAMPO VIGENTE COMO FALSO EN TODOS LOS REGISTROS DE ASCENSO DE PERSONAL
+
+        //GUARDAR NUEVO ASCENSO
+        data.vigente=true;
+        return await this.ascensoService.createOne(data);
+     }
+     //..................................................................................................
+
+     /**
+     * Petición http que edita un registro según id
+     * @param id 
+     * @param data 
+     * @returns 
+     */
+    @Put(':id')
+    async editOne(
+        @Param('id',ParseIntPipe)
+        id: number,
+        @Body()
+        data: EditAscensoDto
+    ){
+        return await this.ascensoService.editOne(id, data);
+    }
+    //........................................................
+
+     /**
+     * Petición http que elimina un registro según id
+     * @param id 
+     * @returns 
+     */
+      @Delete(':id')
+      async deleteOne(
+          @Param('id', ParseIntPipe)
+          id: number
+      ){
+          return await this.ascensoService.deleteOne(id);
+      }
+}
--- src/ascenso/ascenso.service.ts
+++ src/ascenso/ascenso.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { MoreThan, Repository } from 'typeorm';
 import { Ascenso } from './entities/ascenso.entity';
 import { EditAscensoDto } from './dto/edit-ascenso-dto';
 import { CreateAscensoDto } from './dto/create-ascenso-dto';
@@ -32,6 +32,66 @@ export class AscensoService {
         return await this.ascensoRepository.findOneOrFail(id);
     }
 
+    //ASCENSOS X LEGAJO
+    async getAscensosXLegajo(legajox: number){
+        try {
+           return await this.ascensoRepository.findAndCount({
+            where: [{legajo: legajox}],
+            order:{
+                id_ascenso: "DESC"
+            }
+        });
+            
+        } catch (error) {
+            throw new BadRequestException(error.message);
+        }
+    }
+    //FIN ASCENSOS X LEGAJO.......................................................
+
+    //ASCENSO VIGENTE X LEGAJO
+    async getAscensoVigenteXLegajo(legajox: number){
+        try {
+            return await this.ascensoRepository.findOne({
+                    where: [{legajo: legajox, vigente: true}]
+                });
+            // if(!existe){
+            //     throw new Error('No se encontro un traslado vigente');
+            // }
+            //return await this.trasladoRepository.findOneOrFail({legajo: legajox});
+
+        } catch (error) {
+            throw new BadRequestException(error.message);
+        }
+
+        
+    }
+    //FIN ASCENSO VIGENTE X LEGAJO.......................................................
+
+    //ASCENSO VIGENTE X LEGAJO
+    async getAscensosVigentesReOrdenar(ordenx: number, gradox: number){
+        try {
+            return await this.ascensoRepository.find({
+                    where: [
+                        {grado_id: gradox, orden: MoreThan (ordenx), vigente:true}
+                    ],
+                    order:{
+                        orden: "ASC"
+                    }
+
+                });
+            // if(!existe){
+            //     throw new Error('No se encontro un traslado vigente');
+            // }
+            //return await this.trasladoRepository.findOneOrFail({legajo: legajox});
+
+        } catch (error) {
+            throw new BadRequestException(error.message);
+        }
+
+        
+    }
+    //FIN ASCENSO VIGENTE X LEGAJO.......................................................
+
     /**
      * Servicio que edita un registro de la tabla ASCENSO según id
      * @param id 
@@ -43,6 +103,25 @@ export class AscensoService {
         if((await respuesta).affected == 0) throw new NotFoundException("No existe el registro de Ciudad que intenta modificar");
         return respuesta;
     }
+    //.........................................................................
+
+    //QUITAR ASCENSO VIGENTE
+    async quitarAscensoVigente(legajox: number, data: EditAscensoDto){        
+
+        try {
+            const cantidad = await this.ascensoRepository.count({legajo:legajox});
+            const respuesta = await this.ascensoRepository.update({legajo:legajox},data);
+            if (cantidad > 0 && respuesta.affected == 0) throw new NotFoundException('Error: No se ha quitado el estado vigente en ascensos anteriores')
+            
+            return respuesta;
+            
+        } catch (error) {
+              throw new BadRequestException(error.message);
+        }                 
+
+        
+    }
+    //FIN QUITAR ASCENSO VIGENTE
     
     /**
      * Servicio que elimina un registro de la tabla SEXO según id
